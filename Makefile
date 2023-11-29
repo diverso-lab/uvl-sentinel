ff895eaff1c3722542ced141b9c4905e8563b53a
David Romero <davromorg@alum.us.es>
2023-11-29 19:49:06 +0100

    feat: Add UVL python parser files

--- Makefile
+++ Makefile
@@ -3,3 +3,4 @@ ANTLR_JAR = ./antlr-4.13.1-complete.jar
 install:
 	java -jar $(ANTLR_JAR) -Dlanguage=Python3 -o . uvl/UVLPython.g4
 	pip install -r requirements.txt
+	python setup.py build
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from antlr4 import CommonTokenStream, FileStream
-from uvl.UVLPythonLexer import UVLPythonLexer
+from uvl.UVLCustomLexer import UVLCustomLexer
 from uvl.UVLPythonParser import UVLPythonParser
 from antlr4.error.ErrorListener import ErrorListener
 
@@ -9,16 +9,16 @@ from antlr4.error.ErrorListener import ErrorListener
 class CustomErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         if "\\t" in msg:
-            print(f"The UVL has the following warning that prevents reading it :Line {line}:{column} - {msg}")
+            print(f"Warning in UVL: Line {line}:{column} - {msg}")
             return
         else:
-            raise Exception(f"The UVL has the following error that prevents reading it :Line {line}:{column} - {msg}")
+            raise Exception(f"Error in UVL: Line {line}:{column} - {msg}")
 
 # Function to process UVL file
 def process_uvl_file(file_path):
     try:
         input_stream = FileStream(file_path)
-        lexer = UVLPythonLexer(input_stream)
+        lexer = UVLCustomLexer(input_stream)
         lexer.removeErrorListeners()
         lexer.addErrorListener(CustomErrorListener())
 
@@ -31,6 +31,7 @@ def process_uvl_file(file_path):
         print(tree.toStringTree(recog=parser))
         return False, False  # No warning, no exception
     except Exception as e:
+        print(f"Exception in file {file_path}: {e}")  # Print the exception information
         if "warning" in str(e).lower():
             return True, False  # Warning
         else:
@@ -41,12 +42,13 @@ headers = f'{"Dataset":<15} {"File Name":<30} {"Warning":<10} {"Exception":<10}\
 with open("summary.txt", "w") as summary:
     summary.write(headers)
     for i in range(1, 21):
-        dir_path = f"uvl/files/dataset_{i}"
+        dir_path = f"dataset/dataset_{i}"
         if os.path.exists(dir_path):
             for file in os.listdir(dir_path):
                 if file.endswith(".uvl"):
                     file_path = os.path.join(dir_path, file)
+                    print(f"Processing file: {file_path}")  # Print the file being processed
                     warning, exception = process_uvl_file(file_path)
                     summary.write(f'{f"dataset_{i}":<15} {file:<30} {str(warning):<10} {str(exception):<10}\n')
 
-# Additional code to count and print summary can be added here
+print("Summary generated in 'summary.txt'")
