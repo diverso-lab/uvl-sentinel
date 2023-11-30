efd7abeac5a007ffda043b091fe02406eb34f6b3
David Romero <davromorg@alum.us.es>
2023-11-30 06:36:05 +0100

    feat: Add validation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from antlr4.error.ErrorListener import ErrorListener
 # Custom error listener
 class CustomErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        if "\\t" in msg:
+        if "warning" in msg:
             print(f"Warning in UVL: Line {line}:{column} - {msg}")
             return
         else:
@@ -28,18 +28,18 @@ def process_uvl_file(file_path):
         parser.addErrorListener(CustomErrorListener())
 
         tree = parser.featureModel()
-        print(tree.toStringTree(recog=parser))
+        tree_string = tree.toStringTree(recog=parser)
+        # print(tree_string.lower())
+        if "warning" in tree_string.lower():
+            return True, False, ""  # Warning, no exception
         return False, False, ""  # No warning, no exception
     except Exception as e:
         error_message = str(e)
         print(f"Exception in file {file_path}: {error_message}")  # Print the exception information
-        if "warning" in error_message.lower():
-            return True, False, error_message  # Warning with message
-        else:
-            return False, True, error_message  # Exception with message
+        return False, True, error_message  # Exception with message
 
 # Main logic
-headers = f'{"Dataset":<15} {"File Name":<30} {"Warning":<10} {"Exception":<10} {"Message":<50}\n'  # Añadir encabezado para mensaje
+headers = f'{"Dataset":<15} {"File Name":<30} {"Full Path":<50} {"Warning":<10} {"Exception":<10} {"Message":<50}\n'  # Cambiar el orden del encabezado
 with open("syntactic_analysis_report.txt", "w") as summary:
     summary.write(headers)
     for i in range(1, 21):
@@ -50,6 +50,8 @@ with open("syntactic_analysis_report.txt", "w") as summary:
                     file_path = os.path.join(dir_path, file)
                     print(f"Processing file: {file_path}")
                     warning, exception, message = process_uvl_file(file_path)
-                    summary.write(f'{f"dataset_{i}":<15} {file:<30} {str(warning):<10} {str(exception):<10} {message:<50}\n')
+                    summary.write(f'{f"dataset_{i}":<15} {file:<30} {file_path:<50} {str(warning):<10} {str(exception):<10} {message:<50}\n')
 
 print("Summary generated in 'syntactic_analysis_report.txt'")
+
+
